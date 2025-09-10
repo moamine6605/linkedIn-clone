77a5faf81efee299ff4d6bdab9d3231fe42e8212
amine <mo.amine.alt@gmail.com>
2025-09-10 05:13:53 +0100

    creating the header of the login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
 
 function App() {
     return (
         <div className="App">
-            
+            <Routes>
+                <Route path='/login' element={<Login/>}/>
+            </Routes>
         </div>
     )
 }
